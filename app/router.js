08f73e6d17adab7da0bb97cd73882c287df35f47
Muamer Ribica <muamer.ribica@gmail.com>
2014-09-04 16:31:00 +0200

    Init commit
    - empty ember-cli app

---
+++ app/router.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+var Router = Ember.Router.extend({
+  location: EplayENV.locationType
+});
+
+Router.map(function() {
+});
+
+export default Router;
---
+++ tests/helpers/resolver.js
@@ -0,0 +1,9 @@
+import Resolver from 'ember/resolver';
+
+var resolver = Resolver.create();
+
+resolver.namespace = {
+  modulePrefix: 'eplay'
+};
+
+export default resolver;
